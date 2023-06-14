703bb772b085272cbc19893d1b957967743c2b60
Lewis <lewis.kagia@student.moringaschool.com>
2023-06-14 16:40:04 +0300

    arrow function

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+    let num1=2000
+    let num2=100
+    return 20;
+}
+function square(){
+    let x=2
+    return 4;
+}
+function add(){
+    let a=3
+    let b=4
+    return 7;
+}
